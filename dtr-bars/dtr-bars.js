625f7fd6dd683a1e9480f0049ae96a8421dc1b4a
sameersrivastava <sameer.srivastava95@gmail.com>
2016-04-23 20:51:45 -0500

    Have the html layout ready to add new questions to the page as of now. Need to write the js function now.

--- dtr-bars/dtr-bars.js
+++ dtr-bars/dtr-bars.js
@@ -1,72 +1,85 @@
-var chart = new Highcharts.Chart({
-    chart: {
-        renderTo: 'container',
-        animation: false
-    },
-    
-    title: {
-        text: 'Highcharts draggable points demo'
-    },
+var chart = function(title){
+    new Highcharts.Chart({
+        chart: {
+            renderTo: 'container',
+            animation: false
+        },
+        
+        title: {
+            text: title
+        },
 
-    xAxis: {
-        categories: ['Design', 'Technology', 'Research']
-    },
-    // yAxis:{
-    //     min:0,
-    //     max:100
-    // },
-    plotOptions: {
-        series: {
-            point: {
-                events: {
-
-                    drag: function (e) {
-                        // Returning false stops the drag and drops. Example:
-                        /*
-                        if (e.newY > 300) {
-                            this.y = 300;
-                            return false;
-                        }
-                        */
-
-                        $('#drag').html(
-                            'Dragging <b>' + this.series.name + '</b>, <b>' + this.category + '</b> to <b>' + Highcharts.numberFormat(e.y, 2) + '</b>');
-                    },
-                    drop: function () {
-                        $('#drop').html(
-                            'In <b>' + this.series.name + '</b>, <b>' + this.category + '</b> was set to <b>' + Highcharts.numberFormat(this.y, 2) + '</b>');
-                    }
-                }
+        xAxis: {
+            categories: ['Design', 'Technology', 'Research']
+        },
+        yAxis:{
+            title: {
+                text: 'Knowledge'
             },
-            stickyTracking: false
+            min:0,
+            max:100
         },
-        column: {
-            stacking: 'normal'
+        legend: {
+                reversed: true
         },
-        line: {
-            cursor: 'ns-resize'
-        }
-    },
+        plotOptions: {
+            series: {
+                point: {
+                    events: {
 
-    tooltip: {
-        yDecimals: 2
-    },
+                        drag: function (e) {
+                            // Returning false stops the drag and drops. Example:
+                            /*
+                            if (e.newY > 300) {
+                                this.y = 300;
+                                return false;
+                            }
+                            */
 
-    series: [{
-        data: [0, 30, 20],
-        //draggableX: true,
-        draggableY: true,
-        dragMinY: 0,
-        type: 'column',
-        minPointLength: 2,
-        name: 'Knowledge You Want to Gain'
-    }, {
-        data: [5, 10, 12],
-        draggableY: true,
-        dragMinY: 0,
-        type: 'column',
-        minPointLength: 2,
-        name: 'Knowledge You Have'
-    }]
+                            $('#drag').html(
+                                'Dragging <b>' + this.series.name + '</b>, <b>' + this.category + '</b> to <b>' + Highcharts.numberFormat(e.y, 2) + '</b>');
+                        },
+                        drop: function () {
+                            $('#drop').html(
+                                'In <b>' + this.series.name + '</b>, <b>' + this.category + '</b> was set to <b>' + Highcharts.numberFormat(this.y, 2) + '</b>');
+                        }
+                    }
+                },
+                stickyTracking: false
+            },
+            column: {
+                stacking: 'normal'
+            },
+            line: {
+                cursor: 'ns-resize'
+            }
+        },
 
-});
+        tooltip: {
+            yDecimals: 2
+        },
+
+        series: [{
+            data: [10, 30, 20],
+            //draggableX: true,
+            draggableY: true,
+            dragMinY: 0,
+            type: 'column',
+            minPointLength: 2,
+            name: 'Knowledge You Want to Gain'
+        }, {
+            data: [5, 10, 12],
+            draggableY: true,
+            dragMinY: 0,
+            type: 'column',
+            minPointLength: 2,
+            name: 'Knowledge You Have'
+        }]
+
+    });
+},
+researchQuestions = function(){
+
+
+};
+chart('test');
