9a5ff472f1e604887956f930d7d151cd0808fd76
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-04-25 12:40:01 -0500

    Added angular and angular sliders

--- spiderweb/script.js
+++ spiderweb/script.js
@@ -1,80 +1,94 @@
+var w = 500,
+h = 500;
+
+var colorscale = d3.scale.category10();
+
+//Legend titles
+var LegendOptions = ['Things You Know','Things You Want To Know'];
+
+//Options for the Radar chart, other than default
+var mycfg = {
+w: w,
+h: h,
+maxValue: 0.6,
+levels: 6,
+ExtraWidthX: 300
+}
+
+// //Call function to draw the Radar chart
+// //Will expect that data is in %'s
+// RadarChart.draw("#chart", d, mycfg);
+
+var claimCount = 0;
+var claimStart = .3
+var oldProgress = []
+var newProgress = []
+watchArray = []
+
+var app = angular.module('spiderweb', ['ngMaterial']);
+app.controller('MyCntrl', function($scope, $compile) {
+	
+	$scope.addClaim = function () {
+	  
+	  function insertClaim(newClaim) {
+	    if(claimCount == 0) {
+	      $('#chart').show()
+	      $('#message').hide()
+	    }
+	    claimCount++;
+
+	    var item = '<li class="collection-item">' +
+	    '<p class="claim">Claim ' + claimCount + ': ' + newClaim + '</p>' +
+	    '<p>Increase by {{ claim' + claimCount + 'Val * 10}} percent</p>' +
+	    '<md-slider flex="" data-val="{{ claim' + claimCount + 'Val }}" ng-change=updateGraph() md-discrete="" ng-model="claim' + claimCount + 'Val" class="claimSlider" id="claim' + claimCount + '" step="1" min="0" max="10" aria-label="rating">'+
+      	'</md-slider>'+
+      	'</li>'
+
+	    $('.collection').append(item)
+	    $(".claimInput").val('');
+
+	    addClaimVal(claimCount)
+	  }
+
+	  function addClaimVal(claimCount) {
+	    oldProgress.push({axis:"Claim " + claimCount, value:claimStart});
+	    newProgress.push({axis:"Claim " + claimCount, value:claimStart});
+	    watchArray.push("claim" + claimCount + "Val");
+	    resetGraph()
+	    var claims = (angular.element(document.getElementsByClassName('collection')));
+	    $compile(claims)($scope);
+	    $scope.$watchGroup(watchArray, function(newVal, oldVal) { 
+	    	if(newVal[watchArray.length-1]) {
+		    	captureChange(newVal[watchArray.length-1], claimCount) 
+		    }
+		});
+	  }
+
+	  function resetGraph() {
+	    updatedData = [oldProgress, newProgress]
+	    RadarChart.draw("#chart", updatedData, mycfg);
+	  }
+
+	  function captureChange(claimVal, claimCount) {
+	    // var sliderID = $(event.target).attr('id');
+	    var newVal = claimVal/100
+	    //....
+	    for (var i = 0; i < newProgress.length; i++) {
+	      if( newProgress[i]["axis"].indexOf(claimCount) != -1 ) {
+	        newProgress[i]["value"] = oldProgress[i]["value"] + newVal;
+	      }
+	      // newProgress[i]["value"] = claimStart
+	    }
+	    resetGraph()
+	  }
+	  var newClaim = $(".claimInput").val()
+	  insertClaim(newClaim)
+
+
+	}
+});
 
 $(function() {
-	var w = 500,
-		h = 500;
-
-	var colorscale = d3.scale.category10();
-
-	//Legend titles
-	var LegendOptions = ['Things You Know','Things You Want To Know'];
-
-	//Options for the Radar chart, other than default
-	var mycfg = {
-	  w: w,
-	  h: h,
-	  maxValue: 0.6,
-	  levels: 6,
-	  ExtraWidthX: 300
-	}
-
-	// //Call function to draw the Radar chart
-	// //Will expect that data is in %'s
-	// RadarChart.draw("#chart", d, mycfg);
-
-	var claimCount = 0;
-	var claimStart = .3
-	var oldProgress = []
-	var newProgress = []
-
-	$('.addClaim').click(function() {
-		if(claimCount == 0) {
-			$('#chart').show()
-			$('#message').hide()
-		}
-		claimCount++;
-		var newClaim = $(".claimInput").val()
-		var item = '<li class="collection-item">' +
-		'<p class="claim">Claim ' + claimCount + ': ' + newClaim + '</p>' +
-	            '<p class="range-field"><input type="range" value="0" class="claimSlider" id="claim' + claimCount + '" min="0" max="10" /></p>' + 
-	          '</li>'
-		$('.collection').append(item)
-		$(".claimInput").val('');
-
-		addClaimVal(claimCount)
-	});
-
-	function addClaimVal(claimCount) {
-		oldProgress.push({axis:"Claim " + claimCount, value:claimStart});
-		newProgress.push({axis:"Claim " + claimCount, value:claimStart});
-		resetGraph()
-		$('#claim' + claimCount).change(function() {
-			captureChange(event, claimCount)
-		});
-	}
-
-	function resetGraph() {
-		updatedData = [oldProgress, newProgress]
-		console.log(updatedData)
-		RadarChart.draw("#chart", updatedData, mycfg);
-	}
-
-	function captureChange(event, claimCount) {
-		// var sliderID = $(event.target).attr('id');
-		var newVal = ($(event.target).val())/100
-		console.log(newVal)
-		//....
-		for (var i = 0; i < newProgress.length; i++) {
-			if( newProgress[i]["axis"].indexOf(claimCount) != -1 ) {
-				newProgress[i]["value"] = oldProgress[i]["value"] + newVal;
-			}
-			// newProgress[i]["value"] = claimStart
-		}
-		resetGraph()
-	}
-
-	
-
-
 	////////////////////////////////////////////
 	/////////// Initiate legend ////////////////
 	////////////////////////////////////////////
